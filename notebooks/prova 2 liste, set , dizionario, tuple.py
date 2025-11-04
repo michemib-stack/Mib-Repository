3f6e01b5978a624df99fed3e74505ab8c8120743
michemib-stack <michemib@gmail.com>
2025-11-04 08:18:19 +0000

    liste

---
+++ notebooks/prova 2 liste, set , dizionario, tuple.py
@@ -0,0 +1,13 @@
+#liste, set , dizionario, tuple
+# Le liste sono collezioni ordinate e modificabili di elementi
+# I set sono collezioni non ordinate di elementi unici
+# I dizionari sono collezioni di coppie chiave-valore
+# Le tuple sono collezioni ordinate e immutabili di elementi    
+# Esempi di ciascuna struttura dati:
+
+# Lista
+frutti = ['mela', 'banana', 'ciliegia']
+print("Lista di frutti:", frutti)       
+# Aggiunta di un elemento alla lista
+frutti.append('arancia')
+print("Lista dopo l'aggiunta di un frutto:", frutti)
